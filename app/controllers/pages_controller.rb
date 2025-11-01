a6809143d0d354574bfe3afd45aa7f030e4d523d
Monesh-soni-dev <sonimonesh115@gmail.com>
2025-11-01 19:17:25 +0530

    project setup

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+    # simple landing page to exercise layout/header/footer
+  end
+
+  def resume_pdf
+    # Render a self-contained, print-friendly resume view (no application layout)
+    render layout: false
+  end
+end
